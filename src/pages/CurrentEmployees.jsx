ee27ba51dad874fc171d7525509cb5efbc85cd29
citymer <cityhunter8484@gmail.com>
2022-10-13 10:15:13 +0200

    rajout bouton Home et datas

--- src/pages/CurrentEmployees.jsx
+++ src/pages/CurrentEmployees.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BasicTable from '../components/BasicTable';
 import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 const CurrentEmployees = () => {
     return (
@@ -8,6 +9,9 @@ const CurrentEmployees = () => {
             <Header/>
             <div className='contentTable'>
             <BasicTable/>
+            </div>
+            <div className='link'>
+            <Link className='lien' to="/">Home</Link>
             </div>
         </div>
     );
