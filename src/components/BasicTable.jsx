4148c4328472bb7a3e3ec99600091f0c2b78f4f1
citymer <cityhunter8484@gmail.com>
2022-10-15 15:23:00 +0200

    formatage des données du formulaire

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
-import MaterialTable from 'material-table';
-import tableIcons from '../utils/IconProvider.js';
+import React from 'react'
+import MaterialTable from 'material-table'
+import tableIcons from '../utils/IconProvider.js'
 
+const datas = [
+  {
+    firstname: 'olivier',
+    lastname: 'fraise',
+    startdate: '12/10/2022',
+    departement: 'Sales',
+    dateOfBirth: '14/04/1963',
+    street: 'origan',
+    city: 'menthe',
+    state: 'AZ',
+    zipcode: '54123',
+  },
 
-const data = [
-    { firstname:"olivier",lastname:"fraise",startdate:"12/10/2022",
-    departement:"Sales",dateofbirth:"14/04/1963",street:"origan",
-    city:"menthe",state:"AZ",zipcode:"54123"}, 
-
-    { firstname:"eliv",lastname:"laise",startdate:"1/10/2022",
-    departement:"Sales",dateofbirth:"4/04/1963",street:"origan",
-    city:"mouthe",state:"AZ",zipcode:"54123"}]
+  {
+    firstname: 'eliv',
+    lastname: 'laise',
+    startdate: '1/10/2022',
+    departement: 'Sales',
+    dateOfBirth: '4/04/1963',
+    street: 'origan',
+    city: 'mouthe',
+    state: 'AZ',
+    zipcode: '54123',
+  },
+]
 
 const columns = [
-    { title: "Firstname", field: "firstname"},
-    { title: "Lastname", field: "lastname"},
-    { title: "Start Date", field: "startdate"},
-    { title: "Departement", field: "departement"},
-    { title: "Date of Birth", field: "dateofbirth"},
-    { title: "Street", field: "street"},
-    { title: "City", field: "city"},
-    { title: "State", field: "state"},
-    { title: "Zip Code", field: "zipcode"},
-    ]
+  { title: 'Firstname', field: 'firstname' },
+  { title: 'Lastname', field: 'lastname' },
+  { title: 'Start Date', field: 'startdate' },
+  { title: 'Departement', field: 'departement' },
+  { title: 'Date of Birth', field: 'dateOfBirth' },
+  { title: 'Street', field: 'street' },
+  { title: 'City', field: 'city' },
+  { title: 'State', field: 'state' },
+  { title: 'Zip Code', field: 'zipcode' },
+]
 
 const BasicTable = () => {
-    return (
-      <MaterialTable 
-        title="Current Employees" 
-        columns={columns} 
-        data={data} 
-        icons={tableIcons}/>
-    );
-};
+  return (
+    <MaterialTable
+      title="Current Employees"
+      columns={columns}
+      data={datas}
+      icons={tableIcons}
+    />
+  )
+}
 
-export default BasicTable;
+export default BasicTable
--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Select from 'react-select'
 import { useForm, Controller } from 'react-hook-form'
+import { statesName } from '../datas/states'
 
 const Formulaire = () => {
   const {
@@ -18,139 +19,147 @@ const Formulaire = () => {
       zipcode: '',
     },
   })
-
-  const onSubmit = (data) => console.log(data)
-
+  const [form, setForm] = useState({})
+  console.log(form)
+  const onSubmit = (data) => {
+    setForm({
+      firstname: data.firstName,
+      lastname: data.lastName,
+      stardate: data.stardate,
+      departement: data.departement.value,
+      dateOfBirth: data.dateOfBirth,
+      street: data.street,
+      city: data.city,
+      state: data.state.value,
+      zipcode: data.zipcode,
+    })
+  }
+  /*
   useEffect(() => {
     reset()
   }, [isSubmitSuccessful, reset])
-
+*/
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
       {isSubmitSuccessful && <div className="alert">Employee Created! </div>}
-      <p>First Name</p>
-      <input
-        {...register('firstName', {
-          required: 'this is required',
-          minLength: {
-            value: 2,
-            message: 'Min length is 2',
-          },
-          pattern: /[A-Za-z]/,
-        })}
-      />
-      <p className="error">{errors.firstName?.message}</p>
-      <p>last Name</p>
-      <input
-        {...register('lastName', {
-          required: 'this is required',
-          minLength: {
-            value: 2,
-            message: 'Min length is 2',
-          },
-          pattern: /[A-Za-z]/,
-        })}
-      />
-      <p className="error">{errors.lastName?.message}</p>
-      <p>Date of Birth</p>
-      <Controller
-        name="date"
-        control={control}
-        defaultValue=""
-        rules={{
-          required: {
-            value: true,
-            message: 'Enter Date of birth',
-          },
-        }}
-        render={({ field: { ref, ...rest } }) => (
-          <input type="date" {...rest} />
-        )}
-      />
-      <p className="error">{errors.date?.message}</p>
-      <p>Start Date</p>
-      <Controller
-        name="stardate"
-        control={control}
-        defaultValue=""
-        rules={{
-          required: {
-            value: true,
-            message: 'Enter Start date',
-          },
-        }}
-        render={({ field: { ref, ...rest } }) => (
-          <input type="date" {...rest} />
-        )}
-      />
-      <p className="error">{errors.stardate?.message}</p>
-      <fieldset>
-        <legend>Address</legend>
-        <p>Street</p>
-        <input {...register('street', { required: 'this is required' })} />
-        <p className="error">{errors.street?.message}</p>
-        <p>City</p>
-        <input {...register('city', { required: 'this is required' })} />
-        <p className="error">{errors.city?.message}</p>
-        <p>State</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <p>First Name</p>
+        <input
+          {...register('firstName', {
+            required: 'this is required',
+            minLength: {
+              value: 2,
+              message: 'Min length is 2',
+            },
+            pattern: /[A-Za-z]/,
+          })}
+        />
+        <p className="error">{errors.firstName?.message}</p>
+        <p>last Name</p>
+        <input
+          {...register('lastName', {
+            required: 'this is required',
+            minLength: {
+              value: 2,
+              message: 'Min length is 2',
+            },
+            pattern: /[A-Za-z]/,
+          })}
+        />
+        <p className="error">{errors.lastName?.message}</p>
+        <p>Date of Birth</p>
         <Controller
-          name="state"
+          name="dateOfBirth"
           control={control}
+          defaultValue=""
           rules={{
             required: {
               value: true,
-              message: 'this is required',
+              message: 'Enter Date of birth',
             },
           }}
+          render={({ field: { ref, ...rest } }) => (
+            <input type="date" {...rest} />
+          )}
+        />
+        <p className="error">{errors.dateOfBirth?.message}</p>
+        <p>Start Date</p>
+        <Controller
+          name="stardate"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: {
+              value: true,
+              message: 'Enter Start date',
+            },
+          }}
+          render={({ field: { ref, ...rest } }) => (
+            <input type="date" {...rest} />
+          )}
+        />
+        <p className="error">{errors.stardate?.message}</p>
+        <fieldset>
+          <legend>Address</legend>
+          <p>Street</p>
+          <input {...register('street', { required: 'Enter your street' })} />
+          <p className="error">{errors.street?.message}</p>
+          <p>City</p>
+          <input {...register('city', { required: 'Enter your city' })} />
+          <p className="error">{errors.city?.message}</p>
+          <p>State</p>
+          <Controller
+            name="state"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: {
+                value: true,
+                message: 'Enter your state',
+              },
+            }}
+            render={({ field }) => <Select {...field} options={statesName} />}
+          />
+          <p className="error">{errors.state?.message}</p>
+          <p>Zip Code</p>
+          <input
+            type="number"
+            {...register('zipcode', {
+              required: 'Enter your zip code',
+              min: 0,
+              max: 999999,
+            })}
+          />
+          <p className="error">{errors.zipcode?.message}</p>
+        </fieldset>
+        <p>Departement</p>
+        <Controller
+          name="departement"
+          defaultValue=""
+          rules={{
+            required: {
+              value: true,
+              message: 'Enter your departement',
+            },
+          }}
+          control={control}
           render={({ field }) => (
             <Select
               {...field}
               options={[
-                { value: 'choco', label: 'Chocolate' },
-                { value: 'strawberry', label: 'Strawberry' },
-                { value: 'vanilla', label: 'Vanilla' },
+                { value: 'sales', label: 'Sales' },
+                { value: 'marketing', label: 'Marketing' },
+                { value: 'engineering', label: 'Engineering' },
+                { value: 'humanresources', label: 'Human Resources' },
+                { value: 'legal', label: 'Legal' },
               ]}
             />
           )}
         />
-        <p className="error">{errors.state?.message}</p>
-        <p>Zip Code</p>
-        <input
-          type="number"
-          {...register('zipcode', {
-            required: 'this is required',
-            min: 0,
-            max: 999999,
-          })}
-        />
-        <p className="error">{errors.zipcode?.message}</p>
-      </fieldset>
-      <p>Departement</p>
-      <Controller
-        name="departement"
-        rules={{
-          required: {
-            value: true,
-            message: 'this is required',
-          },
-        }}
-        control={control}
-        render={({ field }) => (
-          <Select
-            {...field}
-            options={[
-              { value: 'sales', label: 'Sales' },
-              { value: 'marketing', label: 'Marketing' },
-              { value: 'engineering', label: 'Engineering' },
-              { value: 'humanresources', label: 'Human Resources' },
-              { value: 'legal', label: 'Legal' },
-            ]}
-          />
-        )}
-      />
-      <p className="error">{errors.departement?.message}</p>
-      <input type="submit" className="submit" value="Save" />
-    </form>
+        <p className="error">{errors.departement?.message}</p>
+        <input type="submit" className="submit" value="Save" />
+      </form>
+    </div>
   )
 }
 
